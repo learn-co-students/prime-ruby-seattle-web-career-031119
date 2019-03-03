dd2258867e349f96419bd4470662a5a07a4f3f89
Abe D <iaryiou111@gmail.com>
2019-03-03 21:43:24 +0000

    Done.

--- prime.rb
+++ prime.rb
@@ -1,1 +1,13 @@
-# Add  code here!
+def prime?(number)
+  divisors = 0
+  prime = true
+  for i in 1..number
+    if number/i == number.fdiv(i)
+      divisors += 1
+    end
+  end
+  if divisors < 2 || divisors > 2 || number < 0
+    prime = false
+  end
+  prime
+end
